d0df5380c1fb2508330edffc2bedb8ae17f8526a
Keith J. Jones <keith@keithjjones.com>
2016-08-25 11:44:18 -0400

    Added PassiveTotal Enrichment data.

--- libs/pt.py
+++ libs/pt.py
@@ -1,8 +1,8 @@
 #
 # INCLUDES
 #
-import passivetotal.libs.enrichment
 import passivetotal.libs.whois
+import passivetotal.libs.enrichment
 
 #
 # CLASSES
@@ -31,6 +31,19 @@ class PT(object):
         inputheaders.append('PassiveTotal Whois Registrant Name')
         inputheaders.append('PassiveTotal Whois Country')
         inputheaders.append('PassiveTotal Whois Registrar')
+        inputheaders.append('PassiveTotal Enrichment Network')
+        inputheaders.append('PassiveTotal Enrichment Tags')
+        inputheaders.append('PassiveTotal Enrichment Country')
+        inputheaders.append('PassiveTotal Enrichment Longitude')
+        inputheaders.append('PassiveTotal Enrichment Latitude')
+        inputheaders.append('PassiveTotal Enrichment Sinkhole')
+        inputheaders.append('PassiveTotal Enrichment ASN')
+        inputheaders.append('PassiveTotal Enrichment ASN Name')
+        inputheaders.append('PassiveTotal Enrichment Ever Compromised')
+        inputheaders.append('PassiveTotal Enrichment Primary Domain')
+        inputheaders.append('PassiveTotal Enrichment Dynamic DNS')
+        inputheaders.append('PassiveTotal Enrichment Sub Domains')
+        inputheaders.append('PassiveTotal Enrichment Top Level Domain')
 
     """
     Adds the pulled data to the input row.
@@ -82,7 +95,7 @@ class PT(object):
             whoisregistrar = whoisdata['registrar']
         else:
             whoisregistrar = ''
-            
+
         inputrow.append(whoiscontactemail)
         inputrow.append(whoiswhoisserver)
         inputrow.append(whoisregistryupdated)
@@ -92,3 +105,84 @@ class PT(object):
         inputrow.append(whoisregistrantname)
         inputrow.append(whoisregistrantcountry)
         inputrow.append(whoisregistrar)
+            
+        enrichmentdata = self.ptenrichment.get_enrichment(query=host)
+
+        if enrichmentdata.has_key('network'):
+            enrichmentnetwork = enrichmentdata['network']
+        else:
+            enrichmentnetwork = ''
+
+        if enrichmentdata.has_key('tags'):
+            enrichmenttags = '; '.join(enrichmentdata['tags'])
+        else:
+            enrichmenttags = ''
+
+        if enrichmentdata.has_key('country'):
+            enrichmentcountry = enrichmentdata['country']
+        else:
+            enrichmentcountry = ''
+
+        if enrichmentdata.has_key('longitude'):
+            enrichmentlong = enrichmentdata['longitude']
+        else:
+            enrichmentlong = ''
+
+        if enrichmentdata.has_key('latitude'):
+            enrichmentlat = enrichmentdata['latitude']
+        else:
+            enrichmentlat = ''
+
+        if enrichmentdata.has_key('sinkhole'):
+            enrichmentsinkhole = enrichmentdata['sinkhole']
+        else:
+            enrichmentsinkhole = ''
+
+        if enrichmentdata.has_key('autonomousSystemNumber'):
+            enrichmentasn = enrichmentdata['autonomousSystemNumber']
+        else:
+            enrichmentasn = ''
+
+        if enrichmentdata.has_key('autonomousSystemName'):
+            enrichmentasnname = enrichmentdata['autonomousSystemName']
+        else:
+            enrichmentasnname = ''
+
+        if enrichmentdata.has_key('everCompromised'):
+            enrichmentevercompromised = enrichmentdata['everCompromised']
+        else:
+            enrichmentevercompromised = ''
+
+        if enrichmentdata.has_key('primaryDomain'):
+            enrichmentprimarydomain = enrichmentdata['primaryDomain']
+        else:
+            enrichmentprimarydomain = ''
+
+        if enrichmentdata.has_key('dynamicDns'):
+            enrichmentdynamicdns = enrichmentdata['dynamicDns']
+        else:
+            enrichmentdynamicdns = ''
+
+        if enrichmentdata.has_key('subdomains'):
+            enrichmentsubdomains = '; '.join(enrichmentdata['subdomains'])
+        else:
+            enrichmentsubdomains = ''
+
+        if enrichmentdata.has_key('tld'):
+            enrichmenttld = enrichmentdata['tld']
+        else:
+            enrichmenttld = ''
+
+        inputrow.append(enrichmentnetwork)
+        inputrow.append(enrichmenttags)
+        inputrow.append(enrichmentcountry)
+        inputrow.append(enrichmentlong)
+        inputrow.append(enrichmentlat)
+        inputrow.append(enrichmentsinkhole)
+        inputrow.append(enrichmentasn)
+        inputrow.append(enrichmentasnname)
+        inputrow.append(enrichmentevercompromised)
+        inputrow.append(enrichmentprimarydomain)
+        inputrow.append(enrichmentdynamicdns)
+        inputrow.append(enrichmentsubdomains)
+        inputrow.append(enrichmenttld)
