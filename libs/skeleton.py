2b57f138abcf6000b49b791b9538cfe4ee5c6719
Keith J. Jones <keith@keithjjones.com>
2016-08-25 09:48:20 -0400

    Added skeleton intelligence source module.

---
+++ libs/skeleton.py
@@ -0,0 +1,30 @@
+#
+# Skeleton Intelligence Source Module
+#
+
+#
+# INCLUDES
+#
+
+#
+# CLASSES
+#
+
+class Skeleton(object):
+    """
+    Class to hold Skeleton items.
+    """
+    def __init__(self):
+        pass
+    
+    """
+    Adds appropriate headers to input list.
+    """
+    def add_headers(self,inputheaders):
+        pass
+
+    """
+    Adds the pulled data to the input row.
+    """    
+    def add_row(self,host,inputrow):
+        pass
